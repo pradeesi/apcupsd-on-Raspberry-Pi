4555c91b716d180b92f89f06f49f36270d6bb930
pradeesi <pradeep.si@gmail.com>
2016-05-02 09:55:39 +0530

    Added files via upload

---
+++ mqtt_events_publisher.py
@@ -0,0 +1,41 @@
+# Import package
+import argparse
+import paho.mqtt.client as mqtt
+
+# Define Variables
+MQTT_BROKER = "iot.eclipse.org"
+MQTT_TOPIC = "testTopic/APC_UPS_EVENTS"
+
+
+MQTT_PORT = 1883
+MQTT_KEEPALIVE_INTERVAL = 45
+
+# Read command line argument and Publish on MQTT Topic
+parser = argparse.ArgumentParser(description='APC UPS Event Messages')
+parser.add_argument('event_data')
+args = parser.parse_args()
+MQTT_MSG = (args.event_data)
+
+# Define on_connect event Handler
+def on_connect(mosq, obj, rc):
+	print "Connected to MQTT Broker"
+
+# Define on_publish event Handler
+def on_publish(client, userdata, mid):
+	print "Message Published..."
+
+# Initiate MQTT Client
+mqttc = mqtt.Client()
+
+# Register Event Handlers
+mqttc.on_publish = on_publish
+mqttc.on_connect = on_connect
+
+# Connect with MQTT Broker
+mqttc.connect(MQTT_BROKER, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL) 
+
+# Publish message to MQTT Topic 
+mqttc.publish(MQTT_TOPIC,MQTT_MSG)
+
+# Disconnect from MQTT_Broker
+mqttc.disconnect()
